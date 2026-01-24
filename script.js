ce26b5bb4e50658904867f7fadfcaf7f417a4a6d
Rudra Suthar <rudracodelearner@gmail.com>
2026-01-24 11:48:32 +0530

    fixing typewriting bug in js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function initializeTypingEffect() {
     const texts = [
         'Front-End Developer',
         'UI Designer',
-        'Data Analytics Enthusiast'
+        'AI & Data Analytics Enthusiast'
     ];
 
     let textIndex = 0;
@@ -193,4 +193,5 @@ function createScrollToTop() {
         });
     });
 }
+
 // --- FIX ENDS HERE ---
